3f0414b716b4cefe495587bc2fd366b5800f9767
Aayush Yadav <57173240+AAYUSH2002@users.noreply.github.com>
2022-05-10 23:17:36 +0530

    Add files via upload

---
+++ Netflix/assets/js/script.js
@@ -0,0 +1,67 @@
+// QUESTION 1
+const targetDiv = document.getElementById('answer1');
+const btn = document.getElementById('question1');
+btn.onclick = function () {
+    if(targetDiv.style.display !== "block"){
+        targetDiv.style.display = "block";
+    }
+    else{
+        targetDiv.style.display = "none";
+    }
+}
+// QUESTION 2
+const question2 = document.getElementById('answer2');
+const question2btn = document.getElementById('question2');
+question2btn.onclick = function () {
+     if(question2.style.display !== "block"){
+         question2.style.display ="block";
+     }
+     else{
+         question2.style.display = "none";
+     }
+} 
+// QUESTION 3
+const question3 = document.getElementById('answer3');
+const question3btn = document.getElementById('question3');
+question3btn.onclick = function () {
+     if(question3.style.display !== "block"){
+         question3.style.display ="block";
+     }
+     else{
+         question3.style.display = "none";
+     }
+} 
+// QUESTION 4
+const question4 = document.getElementById('answer4');
+const question4btn = document.getElementById('question4');
+question4btn.onclick = function () {
+     if(question4.style.display !== "block"){
+         question4.style.display ="block";
+     }
+     else{
+         question4.style.display = "none";
+     }
+} 
+// QUESTION 5
+const question5 = document.getElementById('answer5');
+const question5btn = document.getElementById('question5');
+question5btn.onclick = function () {
+     if(question5.style.display !== "block"){
+         question5.style.display ="block";
+     }
+     else{
+         question5.style.display = "none";
+     }
+} 
+// QUESTION 6
+const question6 = document.getElementById('answer6');
+const question6btn = document.getElementById('question6');
+question6btn.onclick = function () {
+     if(question6.style.display !== "block"){
+         question6.style.display ="block";
+     }
+     else{
+         question6.style.display = "none";
+     }
+} 
+
